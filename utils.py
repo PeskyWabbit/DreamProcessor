a4f3299bbee1075f2f817158c4e0a666c9b8c4c8
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2018-07-06 17:00:15 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 #All code from https://github.com/lengstrom/fast-style-transfer
 
-mport scipy.misc, numpy as np, os, sys
+import scipy.misc, numpy as np, os, sys
+import urllib.request
+import tarfile
+import zipfile
+
 
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
@@ -11,7 +15,7 @@ def scale_img(style_path, style_scale):
     o0, o1, o2 = scipy.misc.imread(style_path, mode='RGB').shape
     scale = float(style_scale)
     new_shape = (int(o0 * scale), int(o1 * scale), o2)
-    style_target = _get_img(style_path, img_size=new_shape)
+    style_target = get_img(style_path, img_size=new_shape)
     return style_target
 
 def get_img(src, img_size=False):
@@ -32,4 +36,49 @@ def list_files(in_path):
         break
 
     return files
+
+def maybe_download_and_extract(url, download_dir):
+    """
+    Download and extract the data if it doesn't already exist.
+    Assumes the url is a tar-ball file.
+    :param url:
+        Internet URL for the tar-file to download.
+        Example: "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    :param download_dir:
+        Directory where the downloaded file is saved.
+        Example: "data/CIFAR-10/"
+    :return:
+        Nothing.
+    """
+
+    # Filename for saving the file downloaded from the internet.
+    # Use the filename from the URL and add it to the download_dir.
+    filename = url.split('/')[-1]
+    file_path = os.path.join(download_dir, filename)
+
+    # Check if the file already exists.
+    # If it exists then we assume it has also been extracted,
+    # otherwise we need to download and extract it now.
+    if not os.path.exists(file_path):
+        # Check if the download directory exists, otherwise create it.
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir)
+
+        # Download the file from the internet.
+        file_path, _ = urllib.request.urlretrieve(url=url,
+                                                  filename=file_path)
+
+        print()
+        print("Download finished. Extracting files.")
+
+        if file_path.endswith(".zip"):
+            # Unpack the zip-file.
+            zipfile.ZipFile(file=file_path, mode="r").extractall(download_dir)
+        elif file_path.endswith((".tar.gz", ".tgz")):
+            # Unpack the tar-ball.
+            tarfile.open(name=file_path, mode="r:gz").extractall(download_dir)
+
+        print("Done.")
+    else:
+        print("Data has apparently already been downloaded and unpacked.")
 
